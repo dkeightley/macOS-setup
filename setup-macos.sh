96b2e8c606bb38fc4bf3e265d640f21588506f58
Derek <derekjkeightley@gmail.com>
2017-07-01 20:07:04 +1200

    Alter autohide to 0.25

--- setup-macos.sh
+++ setup-macos.sh
@@ -45,7 +45,7 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 # Remove the auto-hiding Dock delay
 defaults write com.apple.dock autohide-delay -float 0
 # Remove the animation when hiding/showing the Dock
-defaults write com.apple.dock autohide-time-modifier -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0.25
 
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
